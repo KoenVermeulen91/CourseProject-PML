33b0a85db6467b36833658f7e5faea4c7e249f24
KoenVermeulen91 <vermeulen.koen@me.com>
2019-11-13 06:27:55 +0100

    1st commit

---
+++ Draft.R
@@ -0,0 +1,7 @@
+### DRAFT ###
+
+# Reading data
+
+train <- read.csv("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv")
+test <- read.csv("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv")
+
